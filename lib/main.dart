3e852cc25e19c331b91020f2971337234de3b348
MumetNgoding <nugroho31.com@gmail.com>
2019-11-08 15:19:39 -0800

    Yuk Ngaji Apps

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './screen/quran_list.dart';
+import './screen/quran_detail.dart';
+import './models/quran_model.dart';
+import './models/quran_ayat_model.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: QuranData(),
+        ),
+        ChangeNotifierProvider.value(
+          value: QuranAyat(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'KuyNgajiApps',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: QuranList(),
+        routes: {
+          '/detail': (ctx) => QuranDetail(),
+        },
+      ),
+    );
+  }
+}
